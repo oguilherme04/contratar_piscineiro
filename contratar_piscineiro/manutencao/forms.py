eff5d528541c5655cc01f8edd16d8b415d13a489
Ivan Junior <ivanjunior077@gmail.com>
2025-09-12 21:56:56 -0300

    correção da piscina e da manutenção

--- contratar_piscineiro/manutencao/forms.py
+++
@@ -1,7 +0,0 @@
-from django.forms import ModelForm
-from .models import Manutencao
-
-class ManutencaoForm(ModelForm):
-    class Meta:
-        model = Manutencao
-        fields = ["piscina", "piscineiro", "equipamento", "tipo", "data_inicio", "data_fim", "observacoes"]
--- contratar_piscineiro/piscina/forms.py
+++
@@ -1,7 +0,0 @@
-from django.forms import ModelForm
-from .models import Piscina
-
-class PiscinaForm(ModelForm):
-    class Meta:
-        model = Piscina
-        fields = ["cliente", "volume", "tipo", "privada"]
